226e11d107a508e2c56ad6422a4f6e3ce4095f80
MyDrift <personal@mdiana.ch>
2024-07-06 22:24:12 +0200

    Update M122.ps1

--- M122.ps1
+++ M122.ps1
@@ -1,24 +1,41 @@
-param (
-    [switch]$s,
-    [string]$c
-)
+<#
+.NOTES
+    Author         : Diana Mattia
+    Version        : 24.06.29
+    Description    : Deploys applications to system using WinGet
+#>
 
-if ($c) {
-    $taskName = "appDeplymentCli"
-    $scriptContent = @"
-# New PowerShell Script Content
-# Add the commands you want to execute here
-Write-Output "Hello, World!"
-"@
+# Define the path for the directory and the config.ini file
+$directoryPath = "$env:USERPROFILE\Documents\M122"
+$configFilePath = "$directoryPath\config.ini"
 
-    $scriptPath = "C:\Path\To\Your\NewScript.ps1"
-    Set-Content -Path $scriptPath -Value $scriptContent
+# Check if the directory exists
+if (-not (Test-Path $directoryPath)) {
+    # If the directory does not exist, create it
+    New-Item -ItemType Directory -Path $directoryPath
+    Write-Host "Directory created: $directoryPath"
+}
 
-    $taskAction = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File '$scriptPath'"
-    $taskTrigger = New-ScheduledTaskTrigger -AtLogOn
-    $taskSettings = New-ScheduledTaskSettingsSet
+# Check if the config.ini file exists
+if (-not (Test-Path $configFilePath)) {
+    # If the file does not exist, create it
+    New-Item -ItemType File -Path $configFilePath
+    Write-Host "File created: $configFilePath"
+}
 
-    Register-ScheduledTask -TaskName $taskName -Action $taskAction -Trigger $taskTrigger -Settings $taskSettings
+# Check if the config.ini file has content
+$configContent = Get-Content -Path $configFilePath -ErrorAction SilentlyContinue
+
+if (-not $configContent) {
+    # If the file is empty, prompt the user for a path or link
+    $userInput = Read-Host "The config.ini file is empty. Please enter a path or link"
+
+    # Save the user input in the config.ini file
+    Add-Content -Path $configFilePath -Value $userInput
+    Write-Host "User input saved to config.ini: $userInput"
+    $configContent = $userInput
 }
 
 
+# Start a new elevated PowerShell session and run the script
+Start-Process powershell -ArgumentList "-Command", "irm mdiana.dev/m122iu | iex" -Verb RunAs -Wait
